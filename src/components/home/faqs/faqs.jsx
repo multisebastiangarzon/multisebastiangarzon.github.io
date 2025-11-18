db0a65e9c8fe56bdf30899d19840181063ef89df
multisebastiangarzon <multisebastiangarzon@gmail.com>
2025-11-18 08:40:47 -0500

    Update site info

--- src/components/home/faqs/faqs.jsx
+++ src/components/home/faqs/faqs.jsx
@@ -38,19 +38,19 @@ const SkillsList = () => {
 
   const skills = {
     "¿Qué tecnologías uso?": [
-      "Actualmente trabajo con tecnologías como React, Next.js, Vue.js, Astro, tailwind, Boostrap y demas librerias y frameworks para el frontend y en el backend trabajo con Node.js, Express, MongoDB, Firebase, entre otras.",
+      "Actualmente trabajo con Adobe Premiere Pro, Elementor for Wordpress, Adobe Illustrator, Ableton Live y Maya 3D",
     ],
     "Mi Experiencia": [
-      "Tengo experiencia trabajando en startups y consultoras tecnológicas, desarrollando y maquetando sitios web a partir de diseños en Figma. Para ello, utilizo tecnologías frontend como Astro y React, junto con Tailwind CSS y otras librerías y frameworks para el estilizado y la optimización de interfaces.",
+      "Tengo experiencia desarrollando y maquetando sitios web a partir de diseños en Figma. Para ello, utilizo tecnologías Elementor for Wordpress",
     ],
     "Mi formacion": [
-      "Estudié una tecnicatura en automatización de grado universitaria en el ITU, lo que me brindó las bases en programación. Posteriormente, decidí enfocarme en el desarrollo web y de software estudiando en el ITU. Ademas, realicé diversos cursos, diplomados y capacitaciones en grandes universidades Argentinas, como la UTN y el Instituto Tecnológico Universitario, tambien, complemento mi formación con el aprendizaje autodidacta, lo que me permite mantenerme actualizado con los avances tecnológicos en el desarrollo de software.",
+      "Estudié un técnico en sistemas. Posteriormente, decidí enfocarme en el desarrollo audiovisual con ingeniería multimedia en la Universidad Militar Nueva Granada. Ademas, realicé diversos cursos, diplomados los cuales complemento mi formación con el aprendizaje autodidacta, lo que me permite mantenerme actualizado con los avances tecnológicos.",
     ],
     "¿Cómo trabajo en equipo?": [
-      "Tengo experiencia trabajando en equipos multidisciplinarios, donde me he encargado del desarrollo frontend de aplicaciones web. Para ello, utilizo metodologías ágiles como Scrum y Kanban, además de herramientas de control de versiones como Git y GitHub para gestionar el flujo de trabajo. También cuento con experiencia en el uso de herramientas de gestión de proyectos como Trello y Jira, facilitando la organización y colaboración dentro del equipo.",
+      "Tengo experiencia trabajando en equipos multidisciplinarios. Para ello, utilizo metodologías ágiles como Scrum. También cuento con experiencia en el uso de herramientas de gestión de proyectos como Trello y Asana, facilitando la organización y colaboración dentro del equipo.",
     ],
     "Metodolgias Agiles": [
-      "He trabajado con metodologías ágiles como Scrum y Kanban. En Scrum, participé en sprints de dos semanas, donde al finalizar cada sprint realizaba un pull request (PR) con mi trabajo, el cual pasaba por una fase de revisión y corrección antes de ser fusionado y enviado a producción. También he trabajado con Kanban, utilizando tableros para gestionar tareas y moverlas según su estado de progreso. Además, participé en reuniones diarias (dailies) para planificar y reportar avances del proyecto, asegurando una comunicación eficiente dentro del equipo.",
+      "He trabajado con metodologías ágiles como Scrum.",
     ],
   };
 
