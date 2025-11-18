bc915f9e4829651fe3f7f86e0fa1774708b747c9
multisebastiangarzon <multisebastiangarzon@gmail.com>
2025-11-18 06:30:37 -0500

    Updaate site info

--- src/components/home/header/typeEfect.jsx
+++ src/components/home/header/typeEfect.jsx
@@ -6,7 +6,7 @@ const TypewriterComponent = () => {
     <div className="notranslate mt-5 text-3xl font-bold">
       <Typewriter
         options={{
-          strings: ["Desarrollo Web", "Frontend", "Backend", "UX/UI"],
+          strings: ["Ingeniero Multimedia"],
           autoStart: true,
           loop: true,
         }}
