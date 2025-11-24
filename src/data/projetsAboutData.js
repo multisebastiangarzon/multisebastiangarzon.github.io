8bb493b722122aaac8f7e468fa4d60ca3d5c0328
multisebastiangarzon <multisebastiangarzon@gmail.com>
2025-11-23 23:25:06 -0500

    Update site info

--- src/data/projetsAboutData.js
+++ src/data/projetsAboutData.js
@@ -5,7 +5,7 @@ export const proyectos = [
   {
     titulo: "Diseño de contenidos",
     descripcion: "Descripción breve del proyecto 1.",
-    imagen: Infinity.png,
+    imagen: src/assets/img-projects/Infinity.png,
     tecnologias: ["Reels", "Post", "Carrusel"],
     demo: "https://www.instagram.com/infinityfarmscol",
   },
