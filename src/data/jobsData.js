9a118ff184a3a24cdfd81ed77460bef242fab20f
multisebastiangarzon <multisebastiangarzon@gmail.com>
2025-11-24 09:04:05 -0500

    Update site info

--- src/data/jobsData.js
+++ src/data/jobsData.js
@@ -3,22 +3,22 @@ export const jobs = [
     title: "Ingeniería en Multimedia - Universidad Militar Nueva Granada",
     time: ["2025"],
     description:
-      "Se desarrolla mes a mes diferentes publicaciones (Post, Reels, Carruseles) para la empresa Infinity Farms, una exportadora de flores.",
+      "Actualmente estos desarrollando la carrera de Ingeniería en multimedia, séptimo semestre",
     list: [
-      "Parte del trabajo desarrollado en werock.space",
-      "Se realiza la recopilación de un material y a partir de este se generan diferentes contenidos durante el mes.",
+      "Con un enfoque hacia el diseño",
+      "Desarrollando trabajo en el semillero enfocado a experiencias degitales",
     ],
   },
   {
-    title: "Técnico en sistemas - SENA",
-    time: ["2024"],
-    description: "Desarrollo y mantenimiento de interfaces de usuario con:",
-    list: ["JavaScript - TypeScript", "React - Next.js", "Tailwind", "Shadcn"],
+    title: "Modelado 3D para videojuegos - SENA",
+    time: ["2022"],
+    description: "Optimización de modelos 3D para videojuegos, con un buen manejo de Maya 3D",
+    list: ["Maya 3D", "Blender", "Unreal", "Unity"],
   },
   {
-    title: "Modelado 3D para videojuegos - SENA",
-    time: ["07/2022 - 08/2022"],
-    description: "Desarrollo y mantenimiento de interfaces de usuario con",
-    list: ["React - React Router - Context api - CSS - Material UI"],
+    title: "Técnico en sistemas - SENA",
+    time: ["2021"],
+    description: "Desarrollé un técnico en sistemas mientras relizaba mi bachillerato",
+    list: ["Mantenimiento de computadores", "Bases en lógica de programación", "Desarrollo en equipo"],
   },
 ];
