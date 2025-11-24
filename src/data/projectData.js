16551a355363163b3f1da0cbd455ee70e897ec94
multisebastiangarzon <multisebastiangarzon@gmail.com>
2025-11-24 12:14:19 -0500

    Update site info

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,16 +1,22 @@
-import imgNote from "../assets/img-projects/note-app.png";
-import urbanStyle from "../assets/img-projects/urbanStyle.png";
-import legalWeb from "../assets/img-projects/legalweb.webp";
-import mediCare from "../assets/img-projects/medicare.jpg";
-import astroFit from "../assets/img-projects/Astrofit.jpg";
-import Verbenas from "../assets/img-projects/challenge-newyze.png";
+import CasaRock from "../assets/img-projects/CasaRock.png";
+import Citarella from "../assets/img-projects/Citarella.png";
+import Halcon from "../assets/img-projects/Halcon.png";
+import Infinity from "../assets/img-projects/Infinity.png";
+import jm from "../assets/img-projects/J&M.png";
+import Werock from "../assets/img-projects/WeRock.png";
+import Kassa from "../assets/img-projects/Kassa.png";
+import PaginaKasa from "../assets/img-projects/PaginaKasa.png";
+import PaginaKassa from "../assets/img-projects/PaginaKassa.png";
+import SanMiguel from "../assets/img-projects/SanMiguel.png";
+import Tirano from "../assets/img-projects/Tirano.png";
+import VJ from "../assets/img-projects/VJ.png";
 
 export const proyectos = [
   {
     titulo: "Página Web Citarella",
     descripcion:
       "App full-stack con funciones CRUD, creacion de tableros y listas, autentificacion de usuarios, desarrollada con MERN stack.",
-    imagen: imgNote.src,
+    imagen: Citarella.src,
     tecnologias: ["React", "Tailwind", "Express", "MongoDB"],
     demo: "https://drcitarella.com",
     categoria: "Desarrollo Web",
@@ -19,7 +25,7 @@ export const proyectos = [
     titulo: "Contenidos Infinity Farms",
     descripcion:
       "E-commerce funcional de ropa y accesorios, con carrito de compras categorias y pasarela de pago desarrollado en React.",
-    imagen: urbanStyle.src,
+    imagen: Infinity.src,
     tecnologias: ["React", "Tailwind", "Firebase"],
     demo: "https://www.instagram.com/infinityfarmscol",
     categoria: "Contenidos",
@@ -28,8 +34,7 @@ export const proyectos = [
     titulo: "Página Web La Kasa X la Ventana",
     descripcion:
       "Ecommerce Full-Stack para ver, comprar y vender tus productos, con login para usuarios y vendedores.",
-    imagen:
-      "https://i.pinimg.com/736x/c9/70/bf/c970bfbc24489e39418b2c1b1f8f74ef.jpg",
+    imagen: PaginaKasa.src,
     tecnologias: ["React", "Node", "Express", "MongoDB"],
     demo: "https://lakasacrea.com/lakasaxlaventana",
     categoria: "Desarrollo Web",
@@ -38,8 +43,7 @@ export const proyectos = [
     titulo: "Página Web J&M Creciendo Contigo",
     descripcion:
       "App que realiza operaciones CRUD para agregar, ver, editar y eliminar ingresos/gastos.",
-    imagen:
-      "https://jmcreciendocontigo.com",
+    imagen: jm.src,
     tecnologias: ["React", "contextAPI"],
     demo: "https://jmcreciendocontigo.com",
     categoria: "Desarrllo Web",
@@ -48,8 +52,7 @@ export const proyectos = [
     titulo: "Contenidos Kassa",
     descripcion:
       "Aplicación hecha con React que utiliza una API para mostrar el clima en tiempo real.",
-    imagen:
-      "https://i.pinimg.com/736x/10/6e/cd/106ecd1bfe5ed1c3defb83ea0f87a334.jpg",
+    imagen: Kassa.src,
     tecnologias: ["React", "contextAPI", "tailwind"],
     demo: "https://www.instagram.com/kassa_129",
     categoria: "Contenidos",
@@ -58,7 +61,7 @@ export const proyectos = [
     titulo: "Landing Kassa Apartamentos",
     descripcion:
       "Web desarrollada con Astro y Tailwind, embudo de marketing para estudios jurídicos.",
-    imagen: legalWeb.src,
+    imagen: PaginaKassa.src,
     tecnologias: ["Astro", "tailwind", "JavaScript"],
     demo: "https://kassaapartamentos.com",
     categoria: "Desarrollo Web",
@@ -67,7 +70,7 @@ export const proyectos = [
     titulo: "Contenidos La Casa Del Rock",
     descripcion:
       "Web desarrollada con Astro y Tailwind, embudo de marketing para Obras Sociales.",
-    imagen: mediCare.src,
+    imagen: CasaRock.src,
     tecnologias: ["Astro", "tailwind", "JavaScript"],
     demo: "https://www.instagram.com/lacasadelrockcajica",
     categoria: "Contenidos",
@@ -76,7 +79,7 @@ export const proyectos = [
     titulo: "Contenidos WeRock.space",
     descripcion:
       "Web desarrollada con Astro y Tailwind, embudo de marketing para asesorias fitness.",
-    imagen: astroFit.src,
+    imagen: Werock.src,
     tecnologias: ["Astro", "tailwind", "JavaScript"],
     demo: "https://www.instagram.com/werock.space",
     categoria: "Contenidos",
@@ -84,8 +87,7 @@ export const proyectos = [
   {
     titulo: "VJ para artistas",
     descripcion: "100 proyectos, realizados con HTML, CSS y JavaScript.",
-    imagen:
-      "https://i.pinimg.com/736x/3b/4f/4e/3b4f4e170fcb1795b88f4941f49df0a2.jpg",
+    imagen: VJ.src,
     tecnologias: ["Next.js", "Tailwind", "GraphQL"],
     demo: "https://www.instagram.com/p/DPHU4SkkUAA",
     categoria: "Producción Audiovisual",
@@ -94,8 +96,7 @@ export const proyectos = [
     titulo: "Landing HalconBot",
     descripcion:
       "App Web para organizar tus proyectos, hecha con React + Vite, TypeScript y Tailwind para los estilos.",
-    imagen:
-      "https://i.pinimg.com/736x/8f/f0/bb/8ff0bbd0e5844ed254bede44522c8338.jpg",
+    imagen: Halcon.src,
     tecnologias: ["React", "Vite", "TypeScript", "Tailwind"],
     demo: "https://halconbot.com/",
     categoria: "Desarrollo Web",
@@ -104,7 +105,7 @@ export const proyectos = [
     titulo: "Contenidos Proyecto San Miguel",
     descripcion:
       "Pagina estatica desarrollada con Astro y tailwind como reto para una startup.",
-    imagen: Verbenas.src,
+    imagen: SanMiguel.src,
     tecnologias: ["Astro", "Tailwind", "JavaScript"],
     demo: "https://www.instagram.com/proyectosanmiguelcajica",
     categoria: "Contenidos",
@@ -113,7 +114,7 @@ export const proyectos = [
     titulo: "Página Web Tirano Fajardo",
     descripcion:
       "API RESTful construida con Express y MongoDB para gestionar tareas con operaciones CRUD, Registro de usuarios y login con JWT.",
-    imagen: "https://miro.medium.com/1*47S4iWVwTicFkFL4-Z4uAA.png",
+    imagen: Tirano.src,
     tecnologias: ["Express", "Node.js", "MongoDB", "Mongoose"],
     demo: "https://tiranofajardo.co",
     categoria: "Desarrollo Web",
