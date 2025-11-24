0766efba5a18bef1bbf509d3c1d539978e4a8191
multisebastiangarzon <multisebastiangarzon@gmail.com>
2025-11-24 00:07:00 -0500

    Update site info

--- src/data/jobsData.js
+++ src/data/jobsData.js
@@ -1,28 +1,28 @@
 export const jobs = [
   {
-    title: "Frontend Developer - Neowyze",
-    time: ["12/2024 - 05/2025"],
+    title: "Desarrollo de contenidos - Infinity Farms",
+    time: ["2025"],
     description:
-      "Mis tareas en Neowyze consisten en desarrollar webs responsivas basadas en diseños proporcionados en Figma, utilizando Astro, React, Tailwind y otras librerías frontend.",
+      "Se desarrolla mes a mes diferentes publicaciones (Post, Reels, Carruseles) para la empresa "Infinity Farms", una exportadora de flores.",
     list: [
-      "Para el flujo de trabajo, utilizo Jira como herramienta de gestión y Git/GitHub como sistema de control de versiones.",
-      "Trabajo bajo la metodología ágil Scrum, con sprints de 2 semanas y dos reuniones diarias (dailies) para planificar el proyecto y presentar avances",
+      "Parte del trabajo desarrollado en werock.space",
+      "Se realiza la recopilación de un material y a partir de este se generan diferentes contenidos durante el mes.",
     ],
   },
   {
-    title: "Frontend Developer - Vegna",
-    time: ["05/2024 - 02/2024"],
+    title: "Ingeniero de sonido - Teosinte",
+    time: ["2024"],
     description: "Desarrollo y mantenimiento de interfaces de usuario con:",
     list: ["JavaScript - TypeScript", "React - Next.js", "Tailwind", "Shadcn"],
   },
   {
-    title: "Frontend Developer - Igrowker",
+    title: "Desarrollo de contenido - La Kasa x la ventana",
     time: ["07/2022 - 08/2022"],
     description: "Desarrollo y mantenimiento de interfaces de usuario con",
     list: ["React - React Router - Context api - CSS - Material UI"],
   },
   {
-    title: " Web Developer - NoCountry",
+    title: "Desarrollados Web - Dr. Citarella",
     time: ["03/2022 - 06/2022"],
     description:
       "Desarrollo de aplicaciones web responsivas MVP fullstack usando para su desarrollo tecnologías como",
